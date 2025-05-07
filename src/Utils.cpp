9f4c9d5d98d9581467ebc09ed73934132e9fc7c2
GosuTek <gosutek@gmail.com>
2025-05-07 19:50:47 +0300

    add dedicated function to read .mtx

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -12,6 +12,10 @@
 
 #define DATA_DIRECTORY "data/"
 
+#ifndef BSR_BLOCK_SIZE
+#	define BSR_BLOCK_SIZE 2
+#endif
+
 struct COOElement
 {
 	uint32_t row = 0;
@@ -20,13 +24,9 @@ struct COOElement
 	float val = 0.f;
 };
 
-static void convert_and_write_bcsr(const std::string& filename)
+static std::vector<COOElement> read_mtx(const std::string& filename, int rows, int cols, int nnz)
 {
 	FILE* f;
-	int   rows = 0;
-	int   cols = 0;
-	int   nnz = 0;
-
 	f = fopen(filename.c_str(), "r");
 
 	if (f == NULL) {
@@ -41,7 +41,7 @@ static void convert_and_write_bcsr(const std::string& filename)
 	if (mm_read_mtx_crd_size(f, &rows, &cols, &nnz) != 0) {
 		throw std::runtime_error("Failed to read matrix size");
 	}
-
+	fclose(f);
 	std::vector<COOElement> coo_vec;
 	coo_vec.reserve(nnz);
 	for (int i = 0; i < nnz; ++i) {
@@ -51,7 +51,22 @@ static void convert_and_write_bcsr(const std::string& filename)
 		coo_e.col--;
 		coo_vec.push_back(coo_e);
 	}
-	fclose(f);
+	return coo_vec;
+}
+
+static void convert_and_write_bcsr(const std::string& filename)
+{
+	int rows = 0;
+	int cols = 0;
+	int nnz = 0;
+
+	if (rows % BSR_BLOCK_SIZE != 0) {  // if the blocks don't fit perfectly
+									   // pad accordingly
+	}
+
+	if (cols % BSR_BLOCK_SIZE != 0) {
+		// pad accordingly
+	}
 
 	// Convert here
 	// Write to binary
