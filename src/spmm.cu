b7ff219efce66397cfa21b7d024ac4363177e9aa
GosuTek <gosutek@gmail.com>
2026-01-07 16:51:13 +0200

    add: Naive element wise GMEM sparse x dense kernel

--- src/spmm.cu
+++ src/spmm.cu
@@ -74,6 +74,26 @@ __device__ inline static void set_elem_cm(float* const a, size_t n_rows, size_t 
 	a[col * n_rows + row] = val;
 }
 
+__global__ void spmm_naive_elemwise_gmem(
+	const uint32_t* __restrict__ row_ptr,
+	const uint32_t* __restrict__ col_idx,
+	const float* __restrict__ val,
+	const float* __restrict__ d,
+	const size_t m,  // sparse rows
+	const size_t k,  // sparse cols
+	const size_t n,  // dense cols
+	float* __restrict__ res)
+{
+	uint32_t x = blockIdx.x * blockDim.x + threadIdx.x;  // col of res
+	uint32_t y = blockIdx.y * blockDim.y + threadIdx.y;  // row of res
+
+	float acc = 0.0f;
+	for (size_t i = row_ptr[y]; i < row_ptr[y + 1]; ++i) {
+		acc += get_elem_cm(d, k, col_idx[i], x) * val[i];
+	}
+	set_elem_rm(res, n, y, x, acc);
+}
+
 __global__ void spmm_naive_elemwise_csc_gmem(
 	const float* __restrict__ a,
 	const uint32_t* __restrict__ col_ptr,
