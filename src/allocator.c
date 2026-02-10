64292c6b09f60efd8f1bedc9da05c82c631a0817
GosuTek <gosutek@gmail.com>
2026-02-10 19:40:03 +0200

    wip: stubs for reserving/releasing and commiting/uncommiting vmem using mmap

--- src/allocator.c
+++ src/allocator.c
@@ -1,5 +1,7 @@
 #include "allocator.h"
 #include "spmm.h"
+#include "stdio.h"  // TODO: remove
+#include <unistd.h>
 
 /*
       * +------------------------------------------------------------------------------+
@@ -7,24 +9,35 @@
       * +------------------------------------------------------------------------------+
 */
 
-inline void mem_arena_create(MemArena** const arena, uint64_t size)
+// TODO:
+// 1. size -> reserve_size
+// 2. add commit_size
+inline static void mem_arena_create(MemArena** const arena, uint64_t size)
 {
 	// TODO: Call virtual memory allocator instead of malloc
 	*arena = (MemArena*)malloc(size);
 	(*arena)->mem_alloc_pos = sizeof **arena;
 }
-inline void mem_arena_destroy(MemArena* arena)
+
+inline static void mem_arena_destroy(MemArena* arena)
 {
 	// TODO: Swap this to virtual memory allocation
 	free(arena);
 }
 
-inline void mem_arena_push(MemArena* const arena, uint64_t size, const void** ptr_out)
+// TODO:
+// 1. size -> reserve_size
+// 2. add commit_size
+inline static void mem_arena_push(MemArena* const arena, uint64_t size, const void** ptr_out)
 {
 	*ptr_out = arena + arena->mem_alloc_pos;
 	arena->mem_alloc_pos += size;
 }
-inline void mem_arena_push_zero(MemArena* const arena, uint64_t size, void** ptr_out)
+
+// TODO:
+// 1. size -> reserve_size
+// 2. add commit_size
+inline static void mem_arena_push_zero(MemArena* const arena, uint64_t size, void** ptr_out)
 {
 	*ptr_out = arena + arena->mem_alloc_pos;
 	arena->mem_alloc_pos += size;
@@ -32,15 +45,30 @@ inline void mem_arena_push_zero(MemArena* const arena, uint64_t size, void** ptr
 	memset(*ptr_out, 0, size);
 }
 
-inline void mem_arena_pop(MemArena* const arena, uint64_t size)
+inline static void mem_arena_pop(MemArena* const arena, uint64_t size)
 {
 	arena->mem_alloc_pos -= size;
 }
 
-inline uint64_t mem_arena_pos_get(const MemArena* const arena)
+inline static uint64_t mem_arena_pos_get(const MemArena* const arena)
 {
 	return arena->mem_alloc_pos;
 }
+
+inline static uint32_t vm_get_page_size()
+{
+	return (uint32_t)sysconf(_SC_PAGESIZE);
+}
+
+// INFO: Just calls mmap
+inline static void vm_reserve();
+// INFO: Just calls munmap
+inline static void vm_release();
+
+// TODO: Figure out a way to commit reserved vmem
+inline static void vm_commit();
+// TODO: Figure out a way to uncommit commited mem
+inline static void vm_uncommit();
 
 /*
       * +------------------------------------------------------------------------------+
@@ -48,12 +76,14 @@ inline uint64_t mem_arena_pos_get(const MemArena* const arena)
       * +------------------------------------------------------------------------------+
 */
 
-SpmmStatus_t exec_ctx_create(ExecutionContext_t* ctx)
+SpmmStatus_t
+	exec_ctx_create(ExecutionContext_t* ctx)
 {
 	if (*ctx) {
 		return SPMM_STATUS_INVALID_VALUE;
 	}
 	mem_arena_create(ctx, GIB(1));
+	printf("%lu\n", (*ctx)->mem_alloc_pos);
 	if (!*ctx) {
 		return SPMM_STATUS_ALLOC_FAILED;
 	}
--- src/include/allocator.h
+++ src/include/allocator.h
@@ -4,6 +4,8 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
 #include "helpers.h"
 #include "spmm.h"
@@ -13,14 +15,24 @@ typedef struct MemArena
 	uint64_t mem_alloc_pos;
 } MemArena;
 
-void mem_arena_create(MemArena** const arena, uint64_t size);
-void mem_arena_destroy(MemArena* arena);
+inline static void mem_arena_create(MemArena** const arena, uint64_t size);
+inline static void mem_arena_destroy(MemArena* arena);
 
-void mem_arena_push(MemArena* const arena, uint64_t size, const void** ptr_out);
-void mem_arena_push_zero(MemArena* const arena, uint64_t size, void** ptr_out);
+inline static void mem_arena_push(MemArena* const arena, uint64_t size, const void** ptr_out);
+inline static void mem_arena_push_zero(MemArena* const arena, uint64_t size, void** ptr_out);
 
-void mem_arena_pop(MemArena* const arena, uint64_t size);
+inline static void mem_arena_pop(MemArena* const arena, uint64_t size);
 
-uint64_t mem_arena_pos_get(const MemArena* const);
+inline static uint64_t mem_arena_pos_get(const MemArena* const);
+
+#if defined(__linux__)
+
+inline static uint32_t vm_get_page_size();
+inline static void     vm_reserve();
+inline static void     vm_release();
+inline static void     vm_commit();
+inline static void     vm_uncommit();
+
+#endif  // __linux__
 
 #endif  // ALLOCATOR_H
