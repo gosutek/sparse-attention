6568e71b9e9137570eda2669d20b0d20253ad140
GosuTek <gosutek@gmail.com>
2026-03-12 14:06:19 +0200

    fix: UB by not passing a smem allocation bsize

--- src/cuda/wrapper.cu
+++ src/cuda/wrapper.cu
@@ -163,16 +163,16 @@ SpmmStatus_t spmm(ExecCtx* ctx, SpMatDescr_t h_sp, DnMatDescr_t h_dn, DnMatDescr
 		}
 	case SPMM_KERNEL_TYPE_NNZWISE_COALESCED_NO_SMEM:
 		{
+			const dim3     block(32);
+			const uint64_t smem_bsize = (block.x / _CONSTANTS_WARP_SIZE) * sizeof *d_dn.val;
 			if (invert == SPMM_KERNEL_NO_INVERT) {
 				const dim3 grid(d_dn.cols, d_sp.rows);
-				const dim3 block(64);
 
-				_k_spmm_coalesced_nnzwise_no_smem<<<grid, block>>>(d_sp.csr.row_ptr, d_sp.csr.col_idx, d_sp.val, d_dn.val, d_sp.rows, d_sp.cols, d_dn.cols, d_res.val);
+				_k_spmm_coalesced_nnzwise_no_smem<<<grid, block, smem_bsize>>>(d_sp.csr.row_ptr, d_sp.csr.col_idx, d_sp.val, d_dn.val, d_sp.rows, d_sp.cols, d_dn.cols, d_res.val);
 			} else {
 				const dim3 grid(d_sp.cols, d_dn.rows);
-				const dim3 block(64);
 
-				_k_ispmm_coalesced_nnzwise_no_smem<<<grid, block>>>(d_dn.val, d_sp.csc.col_ptr, d_sp.csc.row_idx, d_sp.val, d_dn.rows, d_dn.cols, d_sp.cols, d_res.val);
+				_k_ispmm_coalesced_nnzwise_no_smem<<<grid, block, smem_bsize>>>(d_dn.val, d_sp.csc.col_ptr, d_sp.csc.row_idx, d_sp.val, d_dn.rows, d_dn.cols, d_sp.cols, d_res.val);
 			}
 			break;
 		}
